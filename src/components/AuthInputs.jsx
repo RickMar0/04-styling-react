31574c789aa2e4374a340be6e915e474ea600562
RickMar0 <165326554+RickMar0@users.noreply.github.com>
2025-06-18 00:59:21 +0200

    - Fixed warning in console - used $invalid - very cool

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -15,16 +15,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${({invalid}) => invalid ? "#f87171" : "#6b7280"};
+  color: ${({$invalid}) => $invalid ? "#f87171" : "#6b7280"};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({invalid})=> invalid ? "#fed2d2" : "#d1d5db"};
-  color: ${({invalid})=> invalid ? "#ef4444" : "#374151"};
-  border: 1px solid ${({invalid}) => invalid ? "#ef4444" : "#f73f3f"};
+  background-color: ${({$invalid})=> $invalid ? "#fed2d2" : "#d1d5db"};
+  color: ${({$invalid})=> $invalid ? "#ef4444" : "#374151"};
+  border: 1px solid ${({$invalid}) => $invalid ? "#ef4444" : "transparent"};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 
@@ -53,19 +53,21 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer >
         <p>
-          <Label invalid={emailNotValid}>
+          <Label $invalid={emailNotValid}>
             Email
           </Label>
-          <Input invalid={emailNotValid}
+          <Input $invalid={emailNotValid}
             type="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <Label invalid={passwordNotValid}>
+          <Label $invalid={passwordNotValid}>
             Password
           </Label>
-          <Input
+          <Input $invalid={passwordNotValid}
             type="password"
+            onChange={(event) => handleInputChange('password', event.target.value)}
           />
         </p>
       </ControlContainer>
